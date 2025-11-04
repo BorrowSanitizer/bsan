fbfae63be6fcae1e5c35d5443a814954525b5141
Ian McCormack <icmccorm@cs.cmu.edu>
2025-11-04 10:42:12 -0500

    Fixed permission semantics.

--- bsan-driver/src/callbacks.rs
+++ bsan-driver/src/callbacks.rs
@@ -1,7 +1,7 @@
 use bsan_shared::{AccessKind, Permission, PermissionInfo, ProtectorKind};
 use rustc_interface::Config;
-use rustc_middle::mir::{RetagKind, RetagParams};
-use rustc_middle::ty::{self, Mutability, Ty, TyCtxt, TypingEnv};
+use rustc_middle::mir::RetagKind;
+use rustc_middle::ty::{Mutability, Ty, TyCtxt, TypingEnv};
 use rustc_middle::util::Providers;
 use rustc_session::Session;
 
@@ -18,44 +18,49 @@ fn override_queries(_sess: &Session, providers: &mut Providers) {
 
 fn retag_perm<'tcx>(
     tcx: TyCtxt<'tcx>,
-    key: (TypingEnv<'tcx>, Ty<'tcx>, Ty<'tcx>, RetagParams),
+    key: (TypingEnv<'tcx>, Ty<'tcx>, Option<Mutability>, RetagKind),
 ) -> Option<u64> {
-    let (env, pointer_ty, pointee_ty, params) = key;
-    let ty_is_freeze = pointee_ty.is_freeze(tcx, env);
+    let (env, pointee_ty, ref_mutability, kind) = key;
+
     let ty_is_unpin = pointee_ty.is_unpin(tcx, env);
-    let is_protected = params.kind == RetagKind::FnEntry;
-    let info = match pointer_ty.kind() {
-        ty::Ref(_, _, mutability) => {
-            let (perm_kind, access_kind) = match mutability {
-                Mutability::Mut if ty_is_unpin => (
-                    Permission::new_reserved(ty_is_freeze && !params.in_unsafe_cell, is_protected),
-                    Some(AccessKind::Read),
-                ),
-                Mutability::Not if ty_is_freeze => {
-                    if params.in_unsafe_cell {
-                        (Permission::new_cell(), None)
-                    } else {
-                        (Permission::new_frozen(), Some(AccessKind::Read))
-                    }
-                }
-                // Raw pointers never enter this function so they are not handled.
-                // However raw pointers are not the only pointers that take the parent
-                // tag, this also happens for `!Unpin` `&mut`s and interior mutable
-                // `&`s, which are excluded above.
-                _ => return None,
-            };
+    let ty_is_freeze = pointee_ty.is_freeze(tcx, env);
+    let is_protected = kind == RetagKind::FnEntry;
 
-            let protector_kind = is_protected.then_some(ProtectorKind::StrongProtector);
-            PermissionInfo { perm_kind, protector_kind, access_kind }
-        }
+    if matches!(ref_mutability, Some(Mutability::Mut) | None if !ty_is_unpin) {
+        // Mutable reference / Box to pinning type: retagging is a NOP.
+        // FIXME: with `UnsafePinned`, this should do proper per-byte tracking.
+        return None;
+    }
 
-        ty::Adt(def, ..) if def.is_box() => {
-            let protector_kind = is_protected.then_some(ProtectorKind::WeakProtector);
-            let perm_kind =
-                Permission::new_reserved(ty_is_freeze && !params.in_unsafe_cell, is_protected);
-            PermissionInfo { perm_kind, protector_kind, access_kind: None }
+    let perm_kind = if ty_is_freeze {
+        match ref_mutability {
+            // Shared references are frozen.
+            Some(Mutability::Not) => Permission::new_frozen(),
+            // Mutable references and Boxes are reserved.
+            _ => Permission::new_reserved_frz(),
         }
-        _ => return None,
+    } else {
+        match ref_mutability {
+            // Shared references are "transparent".
+            Some(Mutability::Not) => Permission::new_cell(),
+            // *Protected* mutable references and boxes are reserved without regarding for interior mutability.
+            _ if is_protected => Permission::new_reserved_frz(),
+            // Unprotected mutable references and boxes start in `ReservedIm`.
+            _ => Permission::new_reserved_im(),
+        }
     };
+
+    let access_kind = (!perm_kind.is_cell()).then_some(AccessKind::Read);
+
+    let protector_kind = is_protected.then_some(if ref_mutability.is_some() {
+        // Strong protector for references
+        ProtectorKind::StrongProtector
+    } else {
+        // Weak protector for boxes
+        ProtectorKind::WeakProtector
+    });
+
+    let info = PermissionInfo { perm_kind, protector_kind, access_kind };
+
     Some(PermissionInfo::into_raw(info))
 }
