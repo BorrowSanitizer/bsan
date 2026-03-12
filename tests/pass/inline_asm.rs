30e865f7b4a115bd3925b47464569bfdbf300607
Gitter499 <rafayel.amirkhanyan@gmail.com>
2026-03-12 20:43:31 +0000

    fix(#135): Fixed failing test

--- tests/pass/inline_asm.rs
+++ tests/pass/inline_asm.rs
@@ -3,14 +3,18 @@ use std::arch::asm;
 extern "C" {
     fn __bsan_debug_print(ptr: *mut u8);
 }
-//@ revisions: asm-conservative none
+// Note: Using `` as escapes for the directives
+// `@ revisions: asm-conservative none`
 
 // We are conservative by default
-// Note: It seems as though passing in flags to LLVM plugins is not supported by rustc due to versioning
+// `@ [asm-conservative] run`
+// It seems as though passing in flags to LLVM plugins is not supported by rustc due to versioning
 // concerns. See https://internals.rust-lang.org/t/should-rustc-support-custom-llvm-plugin/13807
-// TODO: Investigate this for future reference
-//@ [asm-conservative] compile-flags:
-//@ [none] compile-flags: -Z llvm-plugins=~/.rustup/toolchains/bsan/libbsan_plugin.so -C llvm-args="-bsan-asm-conservative=false" -C passes="bsan-pass"
+// TODO: Investigate this for future reference)
+// `@ [none] compile-flags: -Z llvm-plugins=~/.rustup/toolchains/bsan/libbsan_plugin.so -C llvm-args="-bsan-asm-conservative=false" -C passes="bsan-pass"`
+
+
+//@ run
 fn main() {
     let x: u64;
     unsafe {
