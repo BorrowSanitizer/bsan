405fcf352c9b07fb023d3702529de96420a33734
Oliver Braunsdorf <oliver.braunsdorf@ifi.lmu.de>
2026-03-23 16:25:15 +0000

    WIP3 atomic test

--- tests/pass/provenance_atomic_ir.rs
+++ tests/pass/provenance_atomic_ir.rs
@@ -4,7 +4,7 @@ use std::sync::atomic::AtomicPtr;
 
 #[path = "../utils/bsan_extern.rs"]
 #[macro_use]
-mod bsanutils;
+mod bsan_debug;
 
 unsafe impl Send for Provenance {}
 unsafe impl Sync for Provenance {}
@@ -17,7 +17,7 @@ struct Node {
 
 fn print_provenance_info(addr: *const std::ffi::c_void) {
     unsafe {
-        let shadow_prov_ptr = bsanutils::__bsan_shadow_src(addr) as *const Provenance;
+        let shadow_prov_ptr = bsan_debug::__bsan_shadow_src(addr) as *const Provenance;
         if !shadow_prov_ptr.is_null() {
             let prov = *shadow_prov_ptr;
             println!("Provenance for address {:p}:", addr);
@@ -38,18 +38,13 @@ pub struct Provenance {
 }
 
 macro_rules! get_provenance {
-    ($val:expr) => {{
-        let mut bor_tag: u64 = 0;
-        let mut alloc_info: *mut std::ffi::c_void = std::ptr::null_mut();
+    ($val:expr, $tag:ident, $info:ident) => {
+        let mut $tag: u64 = 0;
+        let mut $info: *mut std::ffi::c_void = std::ptr::null_mut();
         unsafe {
-            let ptr = $val;
-            bsanutils::__bsan_debug_get_provenance($val as *const std::ffi::c_void, &mut bor_tag, &mut alloc_info);
+            bsan_debug::__bsan_debug_get_provenance($val as *const std::ffi::c_void, &mut $tag, &mut $info);
         }
-        Provenance {
-            bor_tag,
-            alloc_info,
-        }
-    }};
+    };
 }
 
 
@@ -73,7 +68,8 @@ fn bla() {
     let mut prov_map = std::collections::HashMap::<UnsafeSend<u32>, Provenance>::new();
     for i in 0..NUM_POINTERS {
         let ptr = &values[i] as *const u32 as *mut u32; // Get a raw pointer to x
-        let prov = get_provenance!(ptr);
+        get_provenance!(ptr, bor_tag, alloc_info);
+        let prov = Provenance { bor_tag, alloc_info };
         println!("Pointer {:p} (value: {}) has provenance: {:?}", ptr, unsafe {*ptr}, prov);
         
         let inserted_ptr = UnsafeSend(ptr);
@@ -88,7 +84,8 @@ fn bla() {
         for (ptr, _) in prov_map.iter() {
             let pointer_storage_ref = &pointer_storage;
             s.spawn(move || {
-                let prov = get_provenance!(ptr.0);
+                get_provenance!(ptr.0, bor_tag, alloc_info);
+                let prov = Provenance { bor_tag, alloc_info };
                 println!("Atomic Pointer storage: storing pointer {:p} with Provenance: {:?}", ptr.0, prov);
                 pointer_storage_ref.store(ptr.0, std::sync::atomic::Ordering::SeqCst);
             });
@@ -98,41 +95,47 @@ fn bla() {
             let pointer_storage_ref = &pointer_storage;
             let prov_map_ref = &prov_map;
             s.spawn(move || {
-                // Check AtomicPtr shadow before loading
+                /*// Check AtomicPtr shadow before loading
                 let storage_addr = pointer_storage_ref as *const AtomicPtr<u32> as *const std::ffi::c_void;
                 unsafe {
-                    let shadow_ptr = bsanutils::__bsan_shadow_src(storage_addr) as *const Provenance;
+                    let shadow_ptr = bsan_debug::__bsan_shadow_src(storage_addr) as *const Provenance;
                     if !shadow_ptr.is_null() {
                         let shadow_prov = *shadow_ptr;
                         println!("Thread {i} before load: AtomicPtr shadow contains: {:?}", shadow_prov);
                     } else {
                         println!("Thread {i} before load: AtomicPtr shadow is null!");
                     }
-                }
+                }*/
                 
+                //let loaded_ptr: *mut u32 = unsafe { *pointer_storage_ref.as_ptr() };
                 let loaded_ptr: *mut u32 = pointer_storage_ref.load(std::sync::atomic::Ordering::SeqCst);
                 if !loaded_ptr.is_null() {
-                    // Check loaded_ptr's shadow memory directly
+                    /*// Check loaded_ptr's shadow memory directly
                     unsafe {
                         let loaded_ptr_shadow_addr = &loaded_ptr as *const _ as *const std::ffi::c_void;
-                        let loaded_ptr_shadow = bsanutils::__bsan_shadow_src(loaded_ptr_shadow_addr) as *const Provenance;
+                        let loaded_ptr_shadow = bsan_debug::__bsan_shadow_src(loaded_ptr_shadow_addr) as *const Provenance;
                         if !loaded_ptr_shadow.is_null() {
                             let shadow_prov = *loaded_ptr_shadow;
                             println!("Thread {i} loaded_ptr variable's shadow contains: {:?}", shadow_prov);
                         } else {
                             println!("Thread {i} loaded_ptr variable's shadow is null!");
                         }
+                    }*/
+
+                    unsafe {
+                        let ptr: *mut u8 = unsafe { core::mem::transmute(loaded_ptr) };
+                        bsan_debug::__bsan_debug_assert_valid(ptr);
                     }
                     
-                    let loaded_ptr_prov = get_provenance!(loaded_ptr);
+                    get_provenance!(loaded_ptr, bor_tag, alloc_info);
                     unsafe {
-                        println!("Thread {i} loading pointer {:p} (value: {}) with Provenance: {:?}", loaded_ptr, *loaded_ptr, loaded_ptr_prov);
+                        println!("Thread {i} loading pointer {:p} (value: {}) with Provenance: {{Tag: {}, AllocInfo: {:p}}}", loaded_ptr, *loaded_ptr, bor_tag, alloc_info);
                     }
                     
                     let orignal_prov = prov_map_ref.get(&UnsafeSend(loaded_ptr)).unwrap();
 
-                    assert_eq!(orignal_prov.bor_tag, loaded_ptr_prov.bor_tag);
-                    assert_eq!(orignal_prov.alloc_info, loaded_ptr_prov.alloc_info);
+                    assert_eq!(orignal_prov.bor_tag, bor_tag);
+                    assert_eq!(orignal_prov.alloc_info, alloc_info);
                 }
             });
         }
