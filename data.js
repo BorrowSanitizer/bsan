abab59496d4dcd05fcb7058072cabe3514e8bf23
github-action-benchmark <github@users.noreply.github.com>
2026-05-18 13:47:34 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for 137196736e9aa5b3b0a7cefbe92129c64555288c

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778641655209,
+  "lastUpdate": 1779112054863,
   "repoUrl": "https://github.com/BorrowSanitizer/bsan",
   "entries": {
     "Benchmarks": [
@@ -31,6 +31,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "hashbrown@0.17.0 (nop) - aarch64-unknown-linux-gnu",
             "value": 6.174005953405436,
+            "unit": "Median Relative Execution Time"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ian McCormack",
+            "username": "icmccorm",
+            "email": "icmccorm@cs.cmu.edu"
+          },
+          "committer": {
+            "name": "Ian McCormack",
+            "username": "icmccorm",
+            "email": "icmccorm@cs.cmu.edu"
+          },
+          "id": "137196736e9aa5b3b0a7cefbe92129c64555288c",
+          "message": "Fixed path.",
+          "timestamp": "2026-05-18T13:42:20Z",
+          "url": "https://github.com/BorrowSanitizer/bsan/commit/137196736e9aa5b3b0a7cefbe92129c64555288c"
+        },
+        "date": 1779112054074,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hashbrown@0.17.0 (nop) - x86_64-unknown-linux-gnu",
+            "value": 5.550762720942577,
+            "unit": "Median Relative Execution Time"
+          },
+          {
+            "name": "hashbrown@0.17.0 (nop) - aarch64-unknown-linux-gnu",
+            "value": 6.05668943594447,
             "unit": "Median Relative Execution Time"
           }
         ]
