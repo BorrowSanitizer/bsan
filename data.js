ab3564a3b358faecbe448f0fa8d7716560ed416b
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 18:43:49 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for 1b921d5219b2d766f27b94855a965daed455862c

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779387111474,
+  "lastUpdate": 1779389029484,
   "repoUrl": "https://github.com/BorrowSanitizer/bsan",
   "entries": {
     "Benchmarks": [
@@ -78,6 +78,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "indexmap@2.14.0 (nop) - x86_64-unknown-linux-gnu",
             "value": 5.805,
+            "unit": "Median Relative Execution Time",
+            "extra": "{\"mode\": \"nop\", \"target\": \"x86_64-unknown-linux-gnu\", \"version\": \"2.14.0\", \"crate\": \"indexmap\"}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "icmccorm@cs.cmu.edu",
+            "name": "Ian McCormack",
+            "username": "icmccorm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1b921d5219b2d766f27b94855a965daed455862c",
+          "message": "Add a new benchmarking pipeline using `github-action-benchmark` (#184)\n\n* Added script for benchmarking relative execution time.\n\n* Fixed path to config.\n\n* Remove untagged images.\n\n* Merge results.\n\n* Added date formatting.\n\n* versioned file names\n\n* Added date to intermediate artifacts.\n\n* Try out action.\n\n* Try docs.\n\n* Adjust permissions.\n\n* Contents.\n\n* Use gh-pages root.\n\n* .\n\n* rm bencher.sh\n\n* Run on push to main.\n\n* Fix merge.\n\n* Try adding additional fields to the output JSON for better filtering.\n\n* Fixed path.\n\n* Move fields into extra.\n\n* Format extra info as a JSON string.\n\n* Improved debug output formatting, and ensured latest version for build.\n\n* Added indexmap.",
+          "timestamp": "2026-05-21T14:35:04-04:00",
+          "tree_id": "e88b19e54db522497bb30a2358a2cd56710706aa",
+          "url": "https://github.com/BorrowSanitizer/bsan/commit/1b921d5219b2d766f27b94855a965daed455862c"
+        },
+        "date": 1779389029109,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hashbrown@0.17.0 (nop) - aarch64-unknown-linux-gnu",
+            "value": 5.84,
+            "unit": "Median Relative Execution Time",
+            "extra": "{\"mode\": \"nop\", \"target\": \"aarch64-unknown-linux-gnu\", \"version\": \"0.17.0\", \"crate\": \"hashbrown\"}"
+          },
+          {
+            "name": "indexmap@2.14.0 (nop) - aarch64-unknown-linux-gnu",
+            "value": 5.96,
+            "unit": "Median Relative Execution Time",
+            "extra": "{\"mode\": \"nop\", \"target\": \"aarch64-unknown-linux-gnu\", \"version\": \"2.14.0\", \"crate\": \"indexmap\"}"
+          },
+          {
+            "name": "hashbrown@0.17.0 (nop) - x86_64-unknown-linux-gnu",
+            "value": 5.545,
+            "unit": "Median Relative Execution Time",
+            "extra": "{\"mode\": \"nop\", \"target\": \"x86_64-unknown-linux-gnu\", \"version\": \"0.17.0\", \"crate\": \"hashbrown\"}"
+          },
+          {
+            "name": "indexmap@2.14.0 (nop) - x86_64-unknown-linux-gnu",
+            "value": 5.845,
             "unit": "Median Relative Execution Time",
             "extra": "{\"mode\": \"nop\", \"target\": \"x86_64-unknown-linux-gnu\", \"version\": \"2.14.0\", \"crate\": \"indexmap\"}"
           }
