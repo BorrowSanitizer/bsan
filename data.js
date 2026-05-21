276b252e5a8e4145bb531d1eb611c380db323883
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 17:08:40 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for 98f985c1ef439ea71a47104537b1b9dcffa5c5d4

---
+++ data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779383320945,
+  "repoUrl": "https://github.com/BorrowSanitizer/bsan",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Ian McCormack",
+            "username": "icmccorm",
+            "email": "icmccorm@cs.cmu.edu"
+          },
+          "committer": {
+            "name": "Ian McCormack",
+            "username": "icmccorm",
+            "email": "icmccorm@cs.cmu.edu"
+          },
+          "id": "98f985c1ef439ea71a47104537b1b9dcffa5c5d4",
+          "message": "Move fields into extra.",
+          "timestamp": "2026-05-18T13:58:03Z",
+          "url": "https://github.com/BorrowSanitizer/bsan/commit/98f985c1ef439ea71a47104537b1b9dcffa5c5d4"
+        },
+        "date": 1779383319561,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hashbrown@0.17.0 (nop) - x86_64-unknown-linux-gnu",
+            "value": 5.6986623481853425,
+            "unit": "Median Relative Execution Time",
+            "extra": "[object Object]"
+          },
+          {
+            "name": "hashbrown@0.17.0 (nop) - aarch64-unknown-linux-gnu",
+            "value": 5.862688720775822,
+            "unit": "Median Relative Execution Time",
+            "extra": "[object Object]"
+          }
+        ]
+      }
+    ]
+  }
+}
