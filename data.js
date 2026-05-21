1f85674dc8d224006b116500293ca445614d8181
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 17:36:48 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for a51b2a054245056e628fe239856de49509418093

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779383320945,
+  "lastUpdate": 1779385008402,
   "repoUrl": "https://github.com/BorrowSanitizer/bsan",
   "entries": {
     "Benchmarks": [
@@ -34,6 +34,40 @@ window.BENCHMARK_DATA = {
             "value": 5.862688720775822,
             "unit": "Median Relative Execution Time",
             "extra": "[object Object]"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ian McCormack",
+            "username": "icmccorm",
+            "email": "icmccorm@cs.cmu.edu"
+          },
+          "committer": {
+            "name": "Ian McCormack",
+            "username": "icmccorm",
+            "email": "icmccorm@cs.cmu.edu"
+          },
+          "id": "a51b2a054245056e628fe239856de49509418093",
+          "message": "Improved debug output formatting, and ensured latest version for build.",
+          "timestamp": "2026-05-21T17:28:55Z",
+          "url": "https://github.com/BorrowSanitizer/bsan/commit/a51b2a054245056e628fe239856de49509418093"
+        },
+        "date": 1779385007803,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hashbrown@0.17.0 (nop) - aarch64-unknown-linux-gnu",
+            "value": 6.205,
+            "unit": "Median Relative Execution Time",
+            "extra": "{\"mode\": \"nop\", \"target\": \"aarch64-unknown-linux-gnu\", \"version\": \"0.17.0\", \"crate\": \"hashbrown\"}"
+          },
+          {
+            "name": "hashbrown@0.17.0 (nop) - x86_64-unknown-linux-gnu",
+            "value": 6.005,
+            "unit": "Median Relative Execution Time",
+            "extra": "{\"mode\": \"nop\", \"target\": \"x86_64-unknown-linux-gnu\", \"version\": \"0.17.0\", \"crate\": \"hashbrown\"}"
           }
         ]
       }
