08b1b81d8ed03535545925148bdc6c27600d8d11
Gitter499 <rafayel.amirkhanyan@gmail.com>
2026-05-26 15:34:28 -0400

    feat: track variadic provenance with __bsan_var_arg_ctr
    - Add __bsan_var_arg_ctr to bsan-rt and reset on uninstrumented boundary
    - Expose TLS counter in BorrowSanitizer pass
    - Calculate and store vararg count before calls in visitCallBase
    - Allocate shadow stack space for variadics in initStack

--- bsan-rt/llvm-wrapper/bsan.h
+++ bsan-rt/llvm-wrapper/bsan.h
@@ -28,6 +28,9 @@ extern SANITIZER_INTERFACE_ATTRIBUTE THREADLOCAL Provenance
 
 extern SANITIZER_INTERFACE_ATTRIBUTE THREADLOCAL uptr __bsan_had_error;
 
+// represents the number of provenance values that correspond to the variadic arguments being passed to the current function
+extern SANITIZER_INTERFACE_ATTRIBUTE THREADLOCAL uptr __bsan_var_arg_ctr;
+
 extern SANITIZER_INTERFACE_ATTRIBUTE atomic_uintptr_t __bsan_bor_tag_ctr;
 
 namespace __bsan {
