3cc74002175864fe9af485b66c4ccb544c6aab7e
Ian McCormack <icmccorm@cs.cmu.edu>
2026-06-07 11:11:13 -0400

    fmt

--- bsan-rt/llvm-wrapper/bsan_flags.cpp
+++ bsan-rt/llvm-wrapper/bsan_flags.cpp
@@ -1,5 +1,5 @@
-#include "bsan.h"
 #include "bsan_flags.h"
+#include "bsan.h"
 #include "sanitizer_common/sanitizer_common.h"
 #include "sanitizer_common/sanitizer_flag_parser.h"
 #include "sanitizer_common/sanitizer_flags.h"
