cb2462d4a21228683e16c94b447ffab0274c33bc
Ian McCormack <icmccorm@cs.cmu.edu>
2026-07-14 19:54:23 -0400

    debug_assert -> assert

--- bsan-rt/src/tree_borrows/refcount.rs
+++ bsan-rt/src/tree_borrows/refcount.rs
@@ -70,7 +70,7 @@ impl RefCount {
     pub fn decrement_nonatomic(&self) -> bool {
         unsafe {
             let count = self.0.as_ptr();
-            debug_assert!(*count > 0, "RefCount decremented below zero");
+            assert!(*count > 0, "RefCount decremented below zero");
             *count -= 1;
             *count == 0
         }
