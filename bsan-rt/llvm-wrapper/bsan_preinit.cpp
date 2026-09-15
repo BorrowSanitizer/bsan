667df705fa43c6454aed2ef2f44ae4159ca7c8a6
Gitter499 <rafayel.amirkhanyan@gmail.com>
2026-09-15 16:07:28 -0400

    fix(#345): Add reference and flags for preinit initialization

--- bsan-rt/llvm-wrapper/bsan_preinit.cpp
+++ bsan-rt/llvm-wrapper/bsan_preinit.cpp
@@ -3,6 +3,9 @@
 
 using namespace __bsan;
 
+// Reference to the preinit file for the linker
+extern "C" void __bsan_preinit_anchor() {}
+
 #if SANITIZER_CAN_USE_PREINIT_ARRAY
 __attribute__((section(".preinit_array"), used)) static auto preinit =
     __bsan_init;
