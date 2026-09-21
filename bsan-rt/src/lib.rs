936d8d2dd5a81065277706314ad3af62919e6017
Ian McCormack <icmccorm@cs.cmu.edu>
2026-09-21 18:55:01 -0400

    Split size.

--- bsan-rt/src/lib.rs
+++ bsan-rt/src/lib.rs
@@ -320,14 +320,15 @@ unsafe extern "C" fn __bsan_retag_impl(
         pin_layout: opt_slice(pin_data, pin_len),
     };
 
+    let offset = Size::from_addr(ptr);
     let prov = if checked {
         unsafe {
-            BorrowTracker::for_access_unchecked(ctx, prov, Size::from_addr(ptr), size, |mut bt| {
+            BorrowTracker::for_access_unchecked(ctx, prov, offset, size, |mut bt| {
                 bt.retag(ctx, retag_info, pc).map(Some)
             })
         }
     } else {
-        BorrowTracker::for_access(ctx, prov, Size::from_addr(ptr), Some(size), |mut bt| {
+        BorrowTracker::for_access(ctx, prov, offset, Some(size), |mut bt| {
             bt.retag(ctx, retag_info, pc).map(Some)
         })
     }
@@ -390,19 +391,20 @@ unsafe extern "C" fn __bsan_write_impl(
 ) {
     debug_bsan!("write", ptr, bor_tag, alloc_info);
     let ctx = unsafe { global_ctx() };
+    let offset = Size::from_addr(ptr);
     let prov = Provenance { bor_tag, alloc_info };
     if checked {
         unsafe {
             BorrowTracker::for_access_unchecked(
                 ctx,
                 prov,
-                Size::from_addr(ptr),
+                offset,
                 access_size,
                 |mut bt| bt.access(ctx, AccessKind::Write, pc),
             )
         }
     } else {
-        BorrowTracker::for_access(ctx, prov, Size::from_addr(ptr), Some(access_size), |mut bt| {
+        BorrowTracker::for_access(ctx, prov, offset, Some(access_size), |mut bt| {
             bt.access(ctx, AccessKind::Write, pc)
         })
     }
@@ -439,11 +441,12 @@ extern "C" fn __bsan_dealloc(
 ) {
     debug_bsan!("dealloc", ptr, bor_tag, alloc_info);
     let ctx = unsafe { global_ctx() };
+    let offset = Size::from_addr(ptr);
     let prov: Provenance = Provenance { bor_tag, alloc_info };
     if checked {
         BorrowTracker::for_alloc(prov, |bt| bt.dealloc(ctx, pc))
     } else {
-        BorrowTracker::for_access(ctx, prov, Size::from_addr(ptr), None, |bt| bt.dealloc(ctx, pc))
+        BorrowTracker::for_access(ctx, prov, offset, None, |bt| bt.dealloc(ctx, pc))
     }
     .unwrap_or_else(|err| ctx.handle_error(err, pc));
 }
