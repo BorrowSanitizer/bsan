df498fbff864e3d8dd8c8dc9682a75466124489b
Ian McCormack <icmccorm@cs.cmu.edu>
2026-09-22 14:40:49 -0400

    Only iterate over running threads.

--- bsan-rt/llvm-wrapper/bsan_thread.h
+++ bsan-rt/llvm-wrapper/bsan_thread.h
@@ -229,9 +229,11 @@ template <typename Fn> inline void ForEachThread(Fn callback, void *arg) {
   } ctx{callback, arg};
   GetThreadRegistry().RunCallbackForEachThreadLocked(
       [](ThreadContextBase *tctx_base, void *raw_ctx) {
-        CallbackArgs *ctx = static_cast<CallbackArgs *>(raw_ctx);
-        BsanThreadContext *tctx = static_cast<BsanThreadContext *>(tctx_base);
-        ctx->callback(tctx->thread, ctx->arg);
+        if(tctx_base->status == ThreadStatusRunning) {
+          CallbackArgs *ctx = static_cast<CallbackArgs *>(raw_ctx);
+          BsanThreadContext *tctx = static_cast<BsanThreadContext *>(tctx_base);
+          ctx->callback(tctx->thread, ctx->arg);
+        }
       },
       &ctx);
 }
